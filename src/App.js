fe0f2d9940765fb8f20090712bcf59b68ce92a86
sergeypavlyk <pavlyk.sergeynik@gmail.com>
2024-05-06 14:59:02 +0300

    integrate fb pixel

--- src/App.js
+++ src/App.js
@@ -3,10 +3,12 @@ import Main from "./components/Main"
 import Infoblock from "./components/Infoblock"
 import Mainblock from "./components/Mainblock"
 import Footer from "./components/Footer"
+import MetaPixel from "./utils/meta/metaPixel"
 
 function App() {
   return (
     <div className='App'>
+      <MetaPixel />
       <Header />
       <Main />
       <Infoblock />
